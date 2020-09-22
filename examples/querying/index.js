bb304f2c09b1845b02537bcda72ae78ae35e15c0
Aral Balkan <mail@ar.al>
2020-09-22 14:41:56 +0100

    Add whereIfTrue for predicate functions; rename value to valueOf

--- examples/querying/index.js
+++ examples/querying/index.js
@@ -25,7 +25,7 @@ console.log('db.people', db.people)
 
 console.log(db.people.where('age').isGreaterThan(35).and('name').is('Aral').get())
 
-console.log(db.people.where('name').is('Aral').or('name').is('Laura Kalbag').get())
+console.log(db.people.whereIsTrue('valueOf.name === "Aral" || (valueOf.name === "Laura Kalbag" && valueOf.age > 50)').get())
 
 
 // console.log(db.people.where('name').is('Aral').or('name').is('Laura Kalbag').get())
