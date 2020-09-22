ea7e09e5647050f058907e6401806b7e9ec40d79
Aral Balkan <mail@ar.al>
2020-09-22 14:30:51 +0100

    Fix ‘or’ connective

--- examples/querying/index.js
+++ examples/querying/index.js
@@ -24,3 +24,8 @@ peopleYoungerThan35[0].name = 'Laura Kalbag'
 console.log('db.people', db.people)
 
 console.log(db.people.where('age').isGreaterThan(35).and('name').is('Aral').get())
+
+console.log(db.people.where('name').is('Aral').or('name').is('Laura Kalbag').get())
+
+
+// console.log(db.people.where('name').is('Aral').or('name').is('Laura Kalbag').get())
