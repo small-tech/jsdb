959bbbf40220b5abe446c8069e54e9a491789e41
Aral Balkan <mail@ar.al>
2020-09-22 12:20:11 +0100

    Refactor WhatDB into separate class in lib

--- examples/querying/index.js
+++ examples/querying/index.js
@@ -2,5 +2,11 @@ const WhatDB = require('../..')
 
 const db = new WhatDB('db')
 
-console.log('===>', db.people.where('name').isEqualTo('Aral')[0])
+// console.log('===>', db.people.where('name').is('Aral')[0].pet='Oskar')
 
+const peopleYoungerThan35 = db.people.where('age').isLessThan(35).get()
+
+console.log(peopleYoungerThan35)
+console.log(peopleYoungerThan35[0])
+// peopleYoungerThan35[0].name = 'Laura Kalbag'
+console.log(db.people)
