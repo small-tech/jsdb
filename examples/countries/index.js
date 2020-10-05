629c97383ac38140f3446c0c33d416b0daca14fb
Aral Balkan <mail@ar.al>
2020-10-05 12:15:19 +0100

    Update countries example to use new syntax

--- examples/countries/index.js
+++ examples/countries/index.js
@@ -3,16 +3,15 @@ const JSDB = require('../..')
 
 // console.log(languages)
 
-const db = new JSDB('db')
+const db = JSDB.open('db')
 
 // If the data has not been populated yet, populate it.
-if (db.countries === undefined) {
+if (!db.countries) {
   const countries = JSON.parse(fs.readFileSync('./countries.json', 'utf-8'))
   db.countries = countries
 }
 
 // Query the data.
-
 const countriesThatSpeakEnglish = db.countries.where('languages').includes('Kurdish').get()
 
 console.log(countriesThatSpeakEnglish)
