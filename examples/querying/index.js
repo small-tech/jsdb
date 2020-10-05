7ee2d753d6a714a1853fc97d6211073a620d33ed
Aral Balkan <mail@ar.al>
2020-10-05 12:56:24 +0100

    Update querying example to use latest syntax

--- examples/querying/index.js
+++ examples/querying/index.js
@@ -1,15 +1,15 @@
 const JSDB = require('../..')
 
-const db = new JSDB('db', { deleteIfExists: true })
+const db = JSDB.open('db')
 
-// Create test/people.json with some data.
-db.people = [
-  {name: 'Aral', age: 43},
-  {name: 'Laura', age: 34},
-  {name: 'Osky', age: 8}
-]
-
-// console.log('===>', db.people.where('name').is('Aral')[0].pet='Oskar')
+// Create test/people.js with some sample data if it doesn’t already exist.
+if (!db.people) {
+  db.people = [
+    {name: 'Aral', age: 43},
+    {name: 'Laura', age: 34},
+    {name: 'Osky', age: 8}
+  ]
+}
 
 const peopleYoungerThan35 = db.people.where('age').isLessThan(35).get()
 
@@ -26,6 +26,7 @@ console.log('db.people', db.people)
 console.log('referencing first record from results', peopleYoungerThan35[0])
 
 console.log('updating first record (should trigger save)')
+
 peopleYoungerThan35[0].name = 'Laura Kalbag'
 
 console.log('db.people', db.people)
@@ -34,8 +35,6 @@ console.log(db.people.where('age').isGreaterThan(35).and('name').is('Aral').get(
 
 console.log(db.people.whereIsTrue('valueOf.name === "Aral" || (valueOf.name === "Laura Kalbag" && valueOf.age > 50)').get())
 
-
 console.log(db.people.where('name').startsWith('a').get())
-
 
 console.log(db.people.where('name').includes('ka').get())
