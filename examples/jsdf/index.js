bc52574263a25c6a894d61601757da98356556bc
Aral Balkan <mail@ar.al>
2020-10-11 15:24:24 +0100

    Initial implementation of JSDF serialiser

---
+++ examples/jsdf/index.js
@@ -0,0 +1,42 @@
+const JSDF = require('../../lib/JSDF')
+const faker = require('faker')
+
+const card = faker.helpers.createCard()
+
+console.log(card)
+
+console.time('serialisation')
+const serialisedCard = JSDF.serialise(card, '_')
+console.timeLog('serialisation')
+console.log(serialisedCard)
+
+////////////////////////////////////////////////////////////
+
+// Custom object.
+
+class Person {
+  constructor (name = 'Jane Doe') {
+    this.name = name
+  }
+  introduceYourself () {
+    console.log(`Hello, I’m ${this.name}.`)
+  }
+}
+
+// Initialise the people table if it doesn’t already exist.
+const people = [
+  new Person('Aral'),
+  new Person('Laura')
+]
+
+console.time('serialise custom objects')
+const serialisedPeople = JSDF.serialise(people, 'folks')
+console.timeLog('serialise custom objects')
+
+console.log(serialisedPeople)
+
+eval(serialisedPeople)
+
+folks[1].introduceYourself()
+
+
